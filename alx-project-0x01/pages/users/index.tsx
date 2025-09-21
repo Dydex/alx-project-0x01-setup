845f08ad0115ac4aaad6362eea7fd4037bfb31b6
Dydex <supremeajala@gmail.com>
2025-09-21 16:47:44 +0100

    Fixed task 3

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -17,7 +17,7 @@ const Users: React.FC<UserPageProps> = ({ posts }) => {
         </h3>
 
         <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6'>
-          {posts?.map((u) => (
+          {posts.map((u) => (
             <UserCard key={u.id} {...u} />
           ))}
         </div>
